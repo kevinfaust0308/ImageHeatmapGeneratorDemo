6808e4e60b49cdebb6e5a5a0586f631850c21653
Kevin Faust <ikevinfaust@gmail.com>
2017-06-18 00:55:47 -0400

    UI, code, examples fix

--- AlgorithmiaHeatmap.py
+++ AlgorithmiaHeatmap.py
@@ -33,15 +33,15 @@ def home_screen():
 
         # to hold all the uploaded image paths on our flask server which we will upload to our api
         uploaded_flask_files = []
-        # names of all our file paths to show on results screen
-        selected_file_paths = []
+        # names of all selected files to show on results screen
+        selected_file_names = []
 
         # check if any files were uploaded
         # even if nothing was sent, we will still have 1 file object with a empty file name
         if request.files.getlist("upload_img")[0].filename:
 
             # add all the file names to our list
-            selected_file_paths += [fileobj.filename for fileobj in request.files.getlist("upload_img")]
+            selected_file_names += [fileobj.filename for fileobj in request.files.getlist("upload_img")]
 
             # upload each locally selected file to our server
             for file_obj in request.files.getlist("upload_img"):
@@ -51,7 +51,7 @@ def home_screen():
                 filename = 'uploads/' + filename
                 uploaded_flask_files.append(filename)
 
-        selected_file_paths += ["(DEMO) " + name for name in request.form.getlist("demo_img")]
+        selected_file_names += ["(DEMO) " + name for name in request.form.getlist("demo_img")]
         uploaded_flask_files += ["static/" + name for name in request.form.getlist('demo_img')]
 
         # TODO: currently assuming at least 1 image is selected/uploaded
@@ -66,7 +66,7 @@ def home_screen():
         tot_time = '{:.2f}'.format(time.time() - start)
 
         # selected_file_paths to display image title and static_heatmap_paths for loading image/download
-        data = zip(selected_file_paths, static_heatmap_paths, avg_preds)
+        data = zip(selected_file_names, static_heatmap_paths, avg_preds)
 
         return render_template('result.html', data=data, tot_time=tot_time)
 
